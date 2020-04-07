82767f46ca9d66f971646975dfc2a1b7ca3d6912
Nhu Nguyen <nhu.n.nguyen@sjsu.edu>
2020-04-06 17:12:03 -0700

    added buttons, fixed ui

--- public/js/main.js
+++ public/js/main.js
@@ -9,10 +9,11 @@ import Queue from './Queue.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
+const canPredict = true;
 
 let model, webcam, labelContainer, maxPredictions;
 const webcamElement = document.getElementById('webcam');
-const URL = "https://teachablemachine.withgoogle.com/models/9neG499OG/";
+const URL = "https://teachablemachine.withgoogle.com/models/VSYRicuFy/";
 
 
 // Promize.all to loadBackgroundSprites() and loadLevel()
@@ -37,9 +38,9 @@ Promise.all([
         const input = setupKeyboard(mario);
 
         console.log(queue.printQueue());
-        const inputLetter = setupAlphabetKeyboard(queue, mario);
+        //const inputLetter = setupAlphabetKeyboard(queue, mario);
 
-            inputLetter.listenTo(window);  
+            //inputLetter.listenTo(window);  
             input.listenTo(window);    
 
         const timer = new Timer(1/60);
@@ -53,15 +54,15 @@ Promise.all([
                 }
                 level.comp.draw(context, camera);
         }
-        var video = document.querySelector('#video');
-        if(navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-        video.srcObject = stream;
-        })
-        .catch(function (error) {
-            console.log("Webcam went wrong");
-        });
-    }
+    //     var video = document.querySelector('#video');
+    //     if(navigator.mediaDevices.getUserMedia) {
+    //     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+    //     video.srcObject = stream;
+    //     })
+    //     .catch(function (error) {
+    //         console.log("Webcam went wrong");
+    //     });
+    // }
         timer.start();
 
         window.init = async function init()
@@ -99,12 +100,23 @@ Promise.all([
 
         window.predict = async function predict()
         {
-            const prediction = await model.predict(webcam.canvas);
-            for (let i = 0; i < maxPredictions; i++)
+            const predictions = await model.predictTopK(webcam.canvas, 4);
+            //console.log(predictions);
+            for (let i = 0; i < 4; i++)
             {
-                const classPrediction = prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+
+                const classPrediction = predictions[i].className + ": " + predictions[i].probability.toFixed(2);
+                if (predictions[0].className == queue.front() && predictions[0].probability.toFixed(2) >= 0.65)
+                {
+                    //console.log(predictions[0].className);
+                    queue.dequeue();
+                    console.log(queue.printQueue());
+                    mario.leap.start();
+                }
+                //const classPrediction = predictions.as1D().argMax();
                 labelContainer.childNodes[i].innerHTML = classPrediction;
-            }
+                //labelContainer.innerHTML = classPrediction;
+             }
         }
 
 });
